fade742ea30493bfdd06de8ee144603e89b4d106
Niyongere Florentin <florentinniyongere.fn@gmail.com>
2018-07-18 12:46:03 -0500

    made a reverse Array

---
+++ HackerRank/ReverseArray.cpp
@@ -0,0 +1,19 @@
+// prints array backward
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+     
+    int N,i=0;
+    cin>>N;
+    int *A = new int[N];
+    while(cin>>A[i++]);
+    while(cout<<A[--N]<<' ' and N);
+    delete[] A;
+    return 0;
+}

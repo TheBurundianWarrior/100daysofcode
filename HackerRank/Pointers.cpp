367f6c9fdbb201c1d641df72e64541d980281310
Niyongere Florentin <florentinniyongere.fn@gmail.com>
2018-07-18 12:48:23 -0500

    Pointers practice

---
+++ HackerRank/Pointers.cpp
@@ -0,0 +1,21 @@
+// this program allows operations for absolute values and normal operation using pointers
+#include <stdio.h>
+
+void update(int *a,int *b) {
+    int sum = *a + *b;
+    int absDifference = *a - *b > 0 ? *a - *b : -(*a - *b);
+    *a = sum;
+    *b = absDifference; 
+
+}
+
+int main() {
+    int a, b;
+    int *pa = &a, *pb = &b;
+    
+    scanf("%d %d", &a, &b);
+    update(pa, pb);
+    printf("%d\n%d", a, b);
+
+    return 0;
+}
